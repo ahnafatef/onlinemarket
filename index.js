dce4dcd39be94d40abdffd768926243adb388d16
ahnafatef <ahnafatefkhan@gmail.com>
2019-12-09 23:47:58 +0600

    assignment 2

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname))
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+
+app.listen(3000, 'localhost', () => {
+    console.log('server started');
+});
